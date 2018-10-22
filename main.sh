9b27dcba6726d52ba69084b5d9512cd61d6aadd0
Lord Odin <34637033+Noriaki-Kakyoin@users.noreply.github.com>
2018-10-21 22:45:18 -0300

    Version Final

--- main.sh
+++ main.sh
@@ -5,44 +5,49 @@
 while [ ${opcion} -gt 0 ]
 do
   case $opcion in
-      1)
-	## AGREGAR USUARIO
-	clear
-	printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Nombre usuario: "
-       	read -p '' nombreUsuario
-	printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Grupo: "
-	read -p '' grupo
-	printf "${BIRed}Usuario-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Contrasena (opcional): "
-	read -p '' contrasena
-	if [ -z "$contrasena" ]
-	then
-	    # Si no hay contrasena
-	    . scripts/nuevoUsuario.sh $grupo $nombreUsuario
-   	 else
-            . scripts/nuevoUsuario.sh $grupo $nombreUsuario $contrasena
-     	fi
-	break;;
-      2)
-        ## MODIFICAR USUARIO
-        echo "2"
-	break;;
-      3)
-        ## ELIMINAR USUARIO
-        clear	
-	printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Eliminar Usuario>${BIYellow} Nombre usuario: "
-	read -p '' nombreUsuario
-	printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Eliminar Usuario>${BIYellow} Eliminar carpeta personal (S/n): "
-	read -p '' opcion
-	if [ "$opcion" = "S" ] || [ "$opcion" = "s" ]
-       	  then
-	    # Si se quiere eliminar la carpeta personal
-	    . scripts/eliminarUsuario.sh $nombreUsuario $opcion
-	  else
-	    . scripts/eliminarUsuario.sh $nombreUsuario
-	fi
-	break;;
-      *)
-	break
-	;;
-     esac
+    1)
+      ## AGREGAR USUARIO
+      clear
+      printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Nombre usuario: "
+      read -p '' nombreUsuario
+        
+      printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Grupo: "
+      read -p '' grupo
+
+      printf "${BIRed}Usuario-ESI${BIBlue}@${BIGreen}Crear Usuario>${BIYellow} Contrasena (opcional): "
+      read -p '' contrasena
+
+      if [ -z "$contrasena" ]
+        then
+           # Si no hay contrasena
+          . scripts/nuevoUsuario.sh $grupo $nombreUsuario
+        else
+           . scripts/nuevoUsuario.sh $grupo $nombreUsuario $contrasena
+      fi
+
+    break;;
+
+    2)
+    . scripts/modificarUsuario.sh
+    break;;
+    3)
+      ## ELIMINAR USUARIO
+      clear
+      printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Eliminar Usuario>${BIYellow} Nombre usuario: "
+      read -p '' nombreUsuario
+
+      printf "${BIRed}Usuarios-ESI${BIBlue}@${BIGreen}Eliminar Usuario>${BIYellow} Eliminar carpeta personal (S/n): "
+      read -p '' opcion
+
+      if [ "$opcion" = "S" ] || [ "$opcion" = "s" ]
+        then
+          # Si se quiere eliminar la carpeta personal
+          . scripts/eliminarUsuario.sh $nombreUsuario $opcion
+        else
+          . scripts/eliminarUsuario.sh $nombreUsuario
+      fi
+    break;;
+    *)
+    break;;
+  esac
 done
